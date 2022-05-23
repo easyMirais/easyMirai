3dccff0da2ef2970782064e56597ff8cdae67879
LinSiLing <ExMikuPro@qq.com>
2022-05-23 17:32:58 +0800

    循环在消息队列头部获取消息并移除

---
+++ Example/3.py
@@ -0,0 +1,12 @@
+import time
+
+import easyMirai
+
+if __name__ == '__main__':
+    # 初始化->绑定QID->获取头部消息队列返回 字典 类型->循环获取
+    mirai = easyMirai.Mirai("http://127.0.0.1", "8080", "INITKEYxWntQVgk", "1901731529", debug=True)
+    print(mirai.begin())
+    while True:
+        time.sleep(2)
+        message = mirai.getFetchMessage()
+        print(message)
