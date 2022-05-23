34e5ad7f0645df94454e255db084079dbc2d63cb
LinSiLing <ExMikuPro@qq.com>
2022-05-23 17:30:05 +0800

    获取消息队列长度

---
+++ Example/2.py
@@ -0,0 +1,8 @@
+import easyMirai
+
+if __name__ == '__main__':
+    # 初始化->绑定QID->获取消息队列长度返回 字典 类型->运行结束
+    mirai = easyMirai.Mirai("http://127.0.0.1", "8080", "INITKEYxWntQVgk", "1901731529", debug=True)
+    print(mirai.begin())
+    messageCount = mirai.getCountMessage()
+    print(messageCount)
