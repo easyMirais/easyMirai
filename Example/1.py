320c5f6b9485b19703668d5de8bbc9551e4f4549
LinSiLing <ExMikuPro@qq.com>
2022-05-23 17:35:45 +0800

    初始化Bot

--- Example/1.py
+++ Example/1.py
@@ -4,7 +4,7 @@ import easyMirai
 
 if __name__ == '__main__':
     # 初始化->绑定QID->注销Session->运行结束
-    mirai = easyMirai.Mirai("http://127.0.0.1", "8080", "INITKEYxWntQVgk", "1901731529", debug=True)
+    mirai = easyMirai.Mirai("http://127.0.0.1", "8080", "123", "123", debug=True)
     print(mirai.begin())
     time.sleep(2)  # 延时两秒后释放session
     mirai.releaseSession()
