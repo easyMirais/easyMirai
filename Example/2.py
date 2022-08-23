ce5a1ef7e6b009b2548ddfae11c23422a946ab25
ExMikuPro <11087531+ExMikuPro@user.noreply.gitee.com>
2022-08-23 22:17:58 +0800

    日产开发

--- Example/2.py
+++
@@ -1,8 +0,0 @@
-from Package import easyMirai
-
-if __name__ == '__main__':
-    # 初始化->绑定QID->获取消息队列长度返回 字典 类型->运行结束
-    mirai = easyMirai.Mirai("http://127.0.0.1", "8080", "123", "123", debug=True)
-    print(mirai.begin())
-    messageCount = mirai.getCountMessage()
-    print(messageCount)
