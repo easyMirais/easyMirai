63fe29193b7a5da206032b10ebb1dc11484f7681
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-02-20 16:02:39 +0800

    Test File

---
+++ easyMirai/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : SFNCO-Studio
+# @Time     : 2023/2/20 16:02
+# @File     : __main__.py
+# @Project  : Deep in easyMirai
+# @Uri      : https://sfnco.com.cn/
+
+
